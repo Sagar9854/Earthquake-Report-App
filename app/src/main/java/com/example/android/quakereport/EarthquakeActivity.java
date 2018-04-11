18d1e831fc50eb11aefa29ea1225156b014d152a
Sagar9854 <sagar.yeah@gmail.com>
2018-04-11 22:20:26 +0530

    Minor Changes

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -20,13 +20,17 @@ import android.content.AsyncTaskLoader;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -43,7 +47,7 @@ public class EarthquakeActivity extends AppCompatActivity
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
     private static final String USGS_REQUEST_URL =
-            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
+            "https://earthquake.usgs.gov/fdsnws/event/1/query";
 
     private QuakeAdapter mAdapter;
 
@@ -112,10 +116,55 @@ public class EarthquakeActivity extends AppCompatActivity
         }
     }
 
+    /**
+     * it is used to set the setting icon and the functionality of the setting ie. the
+     * user can manually enter on what conditions he want to receive the data
+     * based on the parameter and the way of sorting he wants
+     * @param menu
+     * @return boolean
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate( R.menu.main, menu );
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_setting) {
+            Intent settingsIntent = new Intent(this, SettingActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     public Loader<List<QuakeDescription>> onCreateLoader(int i, Bundle bundle) {
+        /**
+         * adding the prefered choices of the user
+         */
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences( this );
+        String minMagnitude = sharedPref.getString(
+                getString( R.string.settings_min_magnitude_key ),
+                getString( R.string.settings_min_magnitude_default )
+        );
+
+        String orderBy = sharedPref.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
+
+        Uri baseUri = Uri.parse( USGS_REQUEST_URL );
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter( "format", "geojson" );
+        uriBuilder.appendQueryParameter( "limit", "10" );
+        uriBuilder.appendQueryParameter( "minmag", minMagnitude );
+        uriBuilder.appendQueryParameter( "orderby", orderBy );
         //Log.e( LOG_TAG, "mLoader: " + "LOADER is loaded here\n");
-        return new EarthquakeLoader(this, USGS_REQUEST_URL);
+        return new EarthquakeLoader(this, uriBuilder.toString());
     }
 
     @Override
